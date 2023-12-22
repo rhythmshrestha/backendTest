190df1b95582420b1d7a0bf305f6b7ca16a8c55b
Rhythm Shrestha <stharhythm84@gmail.com>
2023-12-22 13:06:04 +0545

    utils code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ dotenv.config({
 
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Express Error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at portL: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!", error);
+  });
