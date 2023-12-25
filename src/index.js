e8d61dd5a3d317287d4bc68b8eed223e3ac98215
Rhythm Shrestha <stharhythm84@gmail.com>
2023-12-25 10:43:31 +0545

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
-
-import connectDB from "./db/index.js";
 
 connectDB()
   .then(() => {
@@ -12,7 +12,7 @@ connectDB()
       throw error;
     });
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at portL: ${process.env.PORT}`);
+      console.log(`Server is running at port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
